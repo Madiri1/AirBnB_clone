4812795b7d83bc3759a4f16735cb821557555a7d
Pascal Madiri <psclmadiri9@gmail.com>
2021-11-14 15:24:37 +0000

    added the basemodel module

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,13 @@ from datetime import datetime
 
 # Defines all common attributes/methods for other classes
 class BaseModel:
+	""" initialise a new BaseModel
+
+	Args:
+		*args (any): Unused
+		**kwargs (dict): key/value pairs of attributes
+	"""
+
 	def __init__(self, *args, **kwargs):
 		self.id = str(uuid())
 		self.created_at = datetime.today
